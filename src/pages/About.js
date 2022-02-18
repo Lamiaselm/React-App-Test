3923a5ea270a61b0cfd6425a9fe11bd21a864f0a
SelmaneLamia <hl_selmane@esi.dz>
2022-02-18 14:59:28 +0100

    first push

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos du site</h1>
+      <br />
+      <p>
+        Consectetur officia duis sint aliquip magna. Consectetur amet incididunt
+        aliquip eiusmod et magna eiusmod et cillum do cupidatat sunt do enim. Ex
+        reprehenderit sint tempor quis. Eu non ullamco irure ut adipisicing esse
+        ea cupidatat minim sit. Esse dolore veniam excepteur nostrud ad commodo
+        culpa.
+      </p>
+      <br />
+      <p>
+        Amet sint sint ipsum proident et sint sunt aliqua cupidatat culpa minim
+        nostrud proident nostrud. Anim nostrud irure ea aute sunt veniam
+        voluptate exercitation. Dolore consectetur voluptate dolore in enim elit
+        nostrud veniam dolore id et magna elit. Minim ut in qui culpa mollit id.
+        Mollit et id deserunt id non magna sit sunt excepteur quis ad. Veniam
+        fugiat consectetur ullamco minim irure dolor nostrud labore veniam enim
+        occaecat ipsum proident. Et sunt tempor sit tempor dolor nisi officia
+        adipisicing aute exercitation sint magna.
+      </p>
+    </div>
+  );
+};
+
+export default About;
